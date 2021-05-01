92b697c577f998452bf9f59f5e08a0ae9087fea9
Pabcargam <pabcargam@alum.us.es>
2021-05-01 14:11:59 +0200

    feat: Pruebas selenium
    Se han realizado las pruebas del listado y añadir productos.

--- product/tests_selenium.py
+++ product/tests_selenium.py
@@ -190,3 +190,162 @@ class SeleniumTests(StaticLiveServerTestCase):
         self.driver.find_element(By.CSS_SELECTOR, ".col-sm-3 u").click()
         self.driver.find_element(By.CSS_SELECTOR, ".titleblock").click()
         assert self.driver.find_element(By.CSS_SELECTOR, ".titleblock").text == "Políticas de privacidad"
+
+    
+    def test_listado_no_login(self):
+        self.driver.get(f'{self.live_server_url}/')
+        self.driver.set_window_size(1080, 1036)
+        self.driver.get(f'{self.live_server_url}/product/list')
+        assert self.driver.find_element(By.CSS_SELECTOR, ".mb-3").text == "Inicio de sesión"
+
+
+    def test_listado_usuario_no_activo(self):
+        self.driver.get(f'{self.live_server_url}/')
+        self.driver.set_window_size(1080, 1036)
+        self.driver.find_element(By.LINK_TEXT, "Iniciar sesión").click()
+        self.driver.find_element(By.ID, "id_username").click()
+        self.driver.find_element(By.ID, "id_username").send_keys("Usuario3")
+        self.driver.find_element(By.ID, "id_password").click()
+        self.driver.find_element(By.ID, "id_password").send_keys("eatsyUsuario3PasswordJQSA!=")
+        self.driver.find_element(By.CSS_SELECTOR, ".save").click()
+        self.driver.get(f'{self.live_server_url}/product/list')
+        assert self.driver.find_element(By.CSS_SELECTOR, ".nombreProducto").text == "Información de pago"
+
+    
+    def test_listado_usuario_activo(self):
+        self.driver.get(f'{self.live_server_url}/')
+        self.driver.set_window_size(1080, 1036)
+        # INICIO DE SESIÓN
+        self.driver.find_element(By.LINK_TEXT, "Iniciar sesión").click()
+        self.driver.find_element(By.ID, "id_username").click()
+        self.driver.find_element(By.ID, "id_username").send_keys("Usuario1")
+        self.driver.find_element(By.ID, "id_password").click()
+        self.driver.find_element(By.ID, "id_password").send_keys("eatsyUsuario1PasswordJQSA!=")
+        self.driver.find_element(By.CSS_SELECTOR, ".save").click()
+        self.driver.get(f'{self.live_server_url}/product/list')
+        # EXISTEN LAS CARDS
+        elements = self.driver.find_elements(By.CSS_SELECTOR, ".col-sm-8")
+        assert len(elements) > 0
+        elements = self.driver.find_elements(By.CSS_SELECTOR, ".product-card-inner")
+        assert len(elements) > 0
+        self.driver.get(f'{self.live_server_url}/product/list?page=2')
+        # EXISTEN LAS CARDS EN LA PG 2
+        elements = self.driver.find_elements(By.CSS_SELECTOR, ".col-sm-8")
+        assert len(elements) > 0
+        elements = self.driver.find_elements(By.CSS_SELECTOR, ".product-card-inner")
+        assert len(elements) > 0
+        self.driver.get(f'{self.live_server_url}/product/list')
+        # COMPROBAMOS LOS 2 BOTONES QUE VEN LOS USUARIOS
+        elements = self.driver.find_elements(By.CSS_SELECTOR, ".btn-md")
+        assert len(elements) > 0
+        assert self.driver.find_element(By.ID, "dropdownMenuButton").text == "Borrar filtros"
+        elements = self.driver.find_elements(By.ID, "filtros-form")
+        assert len(elements) > 0
+        self.driver.get(f'{self.live_server_url}/product/list?titulo=Natillas&dietas=3&orderBy=newest')
+        # EXISTEN LAS CARDS PARA UN DADO NOMBRE BUSCADO
+        elements = self.driver.find_elements(By.CSS_SELECTOR, ".col-sm-8")
+        assert len(elements) > 0
+        elements = self.driver.find_elements(By.CSS_SELECTOR, ".product-card-inner")
+        assert len(elements) > 0
+
+
+    def test_listado_admin(self):
+        self.driver.get(f'{self.live_server_url}/')
+        self.driver.set_window_size(1080, 1036)
+        # INICIO DE SESIÓN
+        self.driver.find_element(By.LINK_TEXT, "Iniciar sesión").click()
+        self.driver.find_element(By.ID, "id_username").click()
+        self.driver.find_element(By.ID, "id_username").send_keys("admin")
+        self.driver.find_element(By.ID, "id_password").click()
+        self.driver.find_element(By.ID, "id_password").send_keys("eatsyAdminPasswordJQSA!=1")
+        self.driver.find_element(By.CSS_SELECTOR, ".save").click()
+        self.driver.get(f'{self.live_server_url}/product/list')
+        # EXISTEN LAS CARDS
+        elements = self.driver.find_elements(By.CSS_SELECTOR, ".col-sm-8")
+        assert len(elements) > 0
+        elements = self.driver.find_elements(By.CSS_SELECTOR, ".product-card-inner")
+        assert len(elements) > 0
+        self.driver.get(f'{self.live_server_url}/product/list?page=2')
+        # EXISTEN LAS CARDS EN LA PG 2
+        elements = self.driver.find_elements(By.CSS_SELECTOR, ".col-sm-8")
+        assert len(elements) > 0
+        elements = self.driver.find_elements(By.CSS_SELECTOR, ".product-card-inner")
+        assert len(elements) > 0
+        self.driver.get(f'{self.live_server_url}/product/list')
+        # COMPROBAMOS LOS 3 BOTONES QUE VE EL ADMINISTRADOR
+        elements = self.driver.find_elements(By.CSS_SELECTOR, ".btn-md")
+        assert len(elements) > 0
+        assert self.driver.find_element(By.ID, "dropdownMenuButton").text == "Borrar filtros"
+        elements = self.driver.find_elements(By.ID, "filtros-form")
+        assert len(elements) > 0
+        self.driver.get(f'{self.live_server_url}/product/list?estado=pendiente')
+        # EXISTEN LAS CARDS PARA LA VISTA DE PRODUCTOS EN REVISION
+        elements = self.driver.find_elements(By.CSS_SELECTOR, ".col-sm-8")
+        assert len(elements) > 0
+        elements = self.driver.find_elements(By.CSS_SELECTOR, ".product-card-inner")
+        assert len(elements) > 0
+        self.driver.get(f'{self.live_server_url}/product/list?titulo=Natillas&dietas=3&orderBy=newest')
+        # EXISTEN LAS CARDS PARA UN DADO NOMBRE BUSCADO
+        elements = self.driver.find_elements(By.CSS_SELECTOR, ".col-sm-8")
+        assert len(elements) > 0
+        elements = self.driver.find_elements(By.CSS_SELECTOR, ".product-card-inner")
+        assert len(elements) > 0
+
+
+    def test_añadir_producto_no_login(self):
+        self.driver.get(f'{self.live_server_url}/')
+        self.driver.set_window_size(1080, 1036)
+        self.driver.get(f'{self.live_server_url}/product/create')
+        assert self.driver.find_element(By.CSS_SELECTOR, ".mb-3").text == "Inicio de sesión"
+
+
+    def test_añadir_producto_usuario_no_activo(self):
+        self.driver.get(f'{self.live_server_url}/')
+        self.driver.set_window_size(1080, 1036)
+        self.driver.find_element(By.LINK_TEXT, "Iniciar sesión").click()
+        self.driver.find_element(By.ID, "id_username").click()
+        self.driver.find_element(By.ID, "id_username").send_keys("Usuario3")
+        self.driver.find_element(By.ID, "id_password").click()
+        self.driver.find_element(By.ID, "id_password").send_keys("eatsyUsuario3PasswordJQSA!=")
+        self.driver.find_element(By.CSS_SELECTOR, ".save").click()
+        self.driver.get(f'{self.live_server_url}/product/create')
+        assert self.driver.find_element(By.CSS_SELECTOR, ".nombreProducto").text == "Información de pago"
+
+    
+    # def test_pruebaCreacion(self):
+    #     self.driver.get(f'{self.live_server_url}/')
+    #     self.driver.set_window_size(1080, 1036)
+    #     # INICIO DE SESIÓN
+    #     self.driver.find_element(By.LINK_TEXT, "Iniciar sesión").click()
+    #     self.driver.find_element(By.ID, "id_username").click()
+    #     self.driver.find_element(By.ID, "id_username").send_keys("admin")
+    #     self.driver.find_element(By.ID, "id_password").click()
+    #     self.driver.find_element(By.ID, "id_password").send_keys("eatsyAdminPasswordJQSA!=1")
+    #     self.driver.find_element(By.CSS_SELECTOR, ".save").click()
+    #     self.driver.find_element(By.CSS_SELECTOR, "#botonAdd > img").click()
+    #     # AÑADIR PRODUCTO
+    #     self.driver.find_element(By.CSS_SELECTOR, ".col-md-3").click()
+    #     self.driver.find_element(By.ID, "id_foto").send_keys("C:/Users/nekom/Desktop/Pablo/Universidad/4º Carrera/SSII/ASIT/CAI/CAI-4/Imagenes/Fichero4.png")
+    #     self.driver.find_element(By.ID, "id_nombre").click()
+    #     self.driver.find_element(By.ID, "id_nombre").send_keys("Prueba")
+    #     self.driver.find_element(By.ID, "id_descripcion").click()
+    #     self.driver.find_element(By.ID, "id_descripcion").send_keys("prueba")
+    #     self.driver.find_element(By.ID, "id_precio").click()
+    #     self.driver.find_element(By.ID, "id_precio").send_keys("5")
+    #     dropdown = self.driver.find_element(By.ID, "id_dieta")
+    #     dropdown.find_element(By.XPATH, "//option[. = 'Vegetariano']").click()
+    #     dropdown = self.driver.find_element(By.ID, "id_dieta")
+    #     dropdown.find_element(By.XPATH, "//option[. = 'Sin gluten']").click()
+    #     self.driver.find_element(By.ID, "id_ubicaciones").click()
+    #     dropdown = self.driver.find_element(By.ID, "id_ubicaciones")
+    #     dropdown.find_element(By.XPATH, "//option[. = 'Lo Vegano']").click()
+    #     self.driver.find_element(By.ID, "id_ubicaciones").click()
+    #     self.driver.find_element(By.CSS_SELECTOR, ".save").click()
+    #     # BUSCAR PRODCUTO RECIEN AÑADIDO COMO PENDIENTE
+    #     self.driver.find_element(By.ID, "dropdownMenuButton").click()
+    #     self.driver.find_element(By.ID, "id_titulo").click()
+    #     self.driver.get(f'{self.live_server_url}/product/list?titulo=Prueba&orderBy=newest&estado=pendiente')
+    #     elements = self.driver.find_elements(By.CSS_SELECTOR, ".col-sm-8")
+    #     assert len(elements) > 0
+    #     elements = self.driver.find_elements(By.CSS_SELECTOR, ".product-card-inner")
+    #     assert len(elements) > 0
